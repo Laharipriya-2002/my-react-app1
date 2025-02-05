0a660ed6bc5de3a5d8089142bea5214d14f7acfa
laharipriya0024 <laharipriya0024@gmail.com>
2025-02-05 14:38:52 +0530

    initial commit

---
+++ my-react-app1/src/footer.jsx
@@ -0,0 +1,13 @@
+// Footer.jsx
+import React from 'react';
+import './footer.css';
+
+const footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Your Company. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default footer;
